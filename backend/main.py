6d480be2d39ba12244e41390d4423f8c388177f2
Ankith-55 <ankithvijayyan@gmail.com>
2026-05-28 22:49:53 +0530

    Final Fullstack Project

--- backend/main.py
+++ backend/main.py
@@ -32,4 +32,4 @@ app.include_router(job.router,prefix=settings.API_PREFIX)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8088,reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8086,reload=True)
